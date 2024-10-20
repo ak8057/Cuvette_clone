01d21f8e0d604b2ad028735c7a9e3730c24d19be
Abhay Kumar <141746531+ak8057@users.noreply.github.com>
2024-10-20 19:10:14 +0530

    Update unsplash.js

--- backend/routes/unsplash.js
+++ backend/routes/unsplash.js
@@ -10,7 +10,7 @@ router.get("/", async (req, res) => {
     const response = await axios.get(`https://api.unsplash.com/search/photos`, {
       params: {
         query: query,
-        client_id: process.env.UNSPLASH_API_KEY, // Access the API key from the environment variables
+        client_id: process.env.UNSPLASH_API_KEY,
       },
       timeout: 10000,
     });
