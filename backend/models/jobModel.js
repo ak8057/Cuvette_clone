7a88623175cef7970333125b9f3045fa79b0b0d7
Abhay Kumar <141746531+ak8057@users.noreply.github.com>
2024-10-20 19:11:53 +0530

    Update jobModel.js

--- backend/models/jobModel.js
+++ backend/models/jobModel.js
@@ -6,25 +6,25 @@ const jobSchema = new mongoose.Schema({
   jobOffer: {
     type: String,
     required: true,
-    enum: ["Full-time", "Part-time", "Freelance", "Internship"], // Restricted to the specified types
+    enum: ["Full-time", "Part-time", "Freelance", "Internship"], 
   },
   location: { type: String, required: true },
   experience: {
     type: String,
-    enum: ["Fresher", "1-3", "3+"], // Experience is restricted to "Fresher", "1-3" or "3+"
+    enum: ["Fresher", "1-3", "3+"], 
   },
   salaryRange: {
-    min: { type: String }, // Optional, for salary range minimum
-    max: { type: String }, // Optional, for salary range maximum
+    min: { type: String }, 
+    max: { type: String }, 
   },
-  skills: [{ type: String }], // Optional, to store array of skills
-  openings: { type: Number }, // Optional, number of available job openings
-  startDate: { type: String }, // Optional, job start date
-  lastDateToApply: { type: String }, // Optional, last date to apply
-  probationPeriod: { type: String }, // Optional, probation period duration
+  skills: [{ type: String }], 
+  openings: { type: Number }, 
+  startDate: { type: String }, 
+  lastDateToApply: { type: String }, 
+  probationPeriod: { type: String }, 
   officeType: {
     type: String,
-    enum: ["In-office", "Remote", "Hybrid"], // Office type is restricted to "In-office", "Remote", or "Hybrid"
+    enum: ["In-office", "Remote", "Hybrid"], 
   },
 });
 
